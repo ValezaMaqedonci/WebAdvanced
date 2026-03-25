581eb9348385b78e9a5a5606047276f5d7c8a3ec
ValezaMaqedonci <lumvaleza@gmail.com>
2026-03-25 16:16:10 +0100

    commit

---
+++ lesson26/main.js
@@ -0,0 +1,22 @@
+console.log('Hello, World!');
+
+var x = 10;
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d);
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 10;
+var i = 4;
+var j = h % i;
+console.log(j);
+
