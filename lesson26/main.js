39f0198a64dde70fa7307f559a3bd00a0b98b9f3
ValezaMaqedonci <lumvaleza@gmail.com>
2026-03-30 16:14:44 +0200

    commit

--- lesson26/main.js
+++ lesson26/main.js
@@ -1,6 +1,6 @@
 console.log('Hello, World!');
 
-var x = 10;
+//var x = 10;
 
 var a = 50 + 50;
 console.log(a);
@@ -20,3 +20,73 @@ var i = 4;
 var j = h % i;
 console.log(j);
 
+var k = 99;
+k++;
+console.log(k);
+
+var l = 113;
+l--;
+console.log(l);
+
+var m = 20;
+var n = m ** 2;
+console.log(n);
+
+var o = 15;
+o+=5;
+console.log(o);
+
+var p = 15;
+p-=5;
+console.log(p);
+
+var q = 15;
+q*=5;
+console.log(q);
+
+var r = 15;
+r/=5;
+console.log(r);
+
+var s = 15;
+var t = 15;
+console.log(s==t);
+
+var u = 125;
+var v = 15;
+console.log(u!=v);
+
+var s = 15;
+var t = 15;
+console.log(s!=t);
+
+var u = 125;
+var v = 15;
+console.log(u==v);
+
+var x = 100;
+var y = 10;
+console.log(x > y);
+
+var nr1 = 1;
+var nr2 = 2;
+console.log(nr1 > nr2);
+
+var w = 1;
+var x = '2';
+console.log(w < x);
+
+var firstName = 'Valeza';
+var lastName = 'Maqedonci';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var school = 'Digital School';
+console.log(`I am a student at ${school}.`);
+
+var age = 23;
+console.log(age >= 15 && age <= 65);
+
+var var1 = 5;
+var var2 = 6;
+console.log(var1 >= 5 || var2 <= 5);
---
+++ lesson27/main.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("This is a message from the showMessage function!");
+}
+
+showMessage();
+
+function sum(nr1, nr2) {
+    return nr1 + nr2;
+}
+
+console.log(sum(5, 10));
