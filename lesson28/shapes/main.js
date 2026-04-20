3359f7d2324da5c1033edaa358e2317c44da1875
ValezaMaqedonci <lumvaleza@gmail.com>
2026-04-20 16:15:05 +0200

    commit

--- lesson28/shapes/main.js
+++ lesson28/shapes/main.js
@@ -6,3 +6,31 @@ var circle = document.getElementById("circle")
 var rect = document.getElementById("rect")
 var tri = document.getElementById("tri")
 
+btn_circle.onclick = function(){
+    circle.setAttribute("class", "shape_circle");
+}
+
+btn_rect.onclick = function(){
+    circle.setAttribute("class", "shape_rect");
+}
+
+btn_tri.onclick = function(){
+    circle.setAttribute("class", "shape_tri");
+}
+
+/*circle.onclick = function()
+{
+    circle.setAttribute("class", "hide");
+}
+
+
+rect.onclick = function()
+{
+    rect.setAttribute("class", "hide");
+}
+
+
+tri.onclick = function()
+{
+    tri.setAttribute("class", "hide");
+}*/
