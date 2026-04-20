2f5bc1b267c4d20ba3da6e54be9162a77fd2f5de
ValezaMaqedonci <lumvaleza@gmail.com>
2026-04-20 15:46:58 +0200

    commit

---
+++ lesson28/shapes/main.js
@@ -0,0 +1,8 @@
+var btn_circle = document.getElementById("btncircle")
+var btn_rect = document.getElementById("btnrect")
+var btn_tri = document.getElementById("btntri")
+
+var circle = document.getElementById("circle")
+var rect = document.getElementById("rect")
+var tri = document.getElementById("tri")
+
